2ba93ea86b449da5768e78f3b3c1fd66e2037651
franklinmatheus <franklinmatheus1@gmail.com>
2021-12-12 21:05:11 -0300

    updating images

--- draw_canvas.py
+++ draw_canvas.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-input = "data_canvas/data2.txt"
-output = "data_canvas/3-3data2.txt"
+input = "data_canvas/data0.txt"
+output = "data_canvas/30-30data0.txt"
 
 indata = open(input, "r", encoding="utf8").read()
 outdata = open(output, "r", encoding="utf8").read()
@@ -11,7 +11,9 @@ outmatrix = [list(map(int, line.strip().split())) for line in outdata.split('\n'
 
 fig, ax = plt.subplots(figsize=(5,5))
 plt.imshow(inmatrix, cmap="gray", interpolation="none")
-plt.axis('off')
+#plt.axis('off')
+plt.tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=False)
+plt.tick_params(axis='y',which='both',left=False,right=False,labelleft=False)
 fig.savefig("images_canvas/in" + input.split("/")[1].split(".")[0] + ".png", dpi=fig.dpi)
 
 plt.imshow(outmatrix, cmap="gray", interpolation="none")
